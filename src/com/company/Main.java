fa3882985ccbb8e52758d99262482a490df916fe
Alexandre <descamps.professionnel@gmail.com>
2018-10-10 15:18:54 +0200

    Moving Game_Vue into the Main class, creation of a class Panel for the Swing Interface

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,24 @@ package com.company;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-	       //Plateau p = new Plateau("lvl1.txt");
-	       Game_Vue g = new Game_Vue();
+    	
+    	JFrame window = new JFrame();
+    	window.setVisible(true);
+    	window.setSize(800, 700);
+    	window.setLocationRelativeTo(null);
+    	window.setTitle("MySokoban");
+    	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	Panel panel = new Panel();
+    	window.add(panel);
+    	
+    	//Plateau p = new Plateau("lvl1.txt");
+	    //Game_Vue g = new Game_Vue();
 	       
     }
 }
