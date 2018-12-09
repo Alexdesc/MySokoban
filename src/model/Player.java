07e0fc5f9ad833549851c7371c90936501785f4f
Avinash <dev.avinash.yogan@gmail.com>
2018-12-09 21:06:26 +0100

    MOdif

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,33 +1,13 @@
 package model;
 
-import javax.swing.*;
-import java.awt.*;
-
 public class Player {
     private Position position;
-    protected int width;
-    protected int height;
-    protected boolean visible;
-    protected Image image;
-
-
+    
     public Player(Position position, String name)
       {
 
         this.position = position;
 
-    }
-
-    protected void loadImage(String imageName) {
-
-        ImageIcon ii = new ImageIcon(imageName);
-        image = ii.getImage();
-    }
-
-    protected void getImageDimensions() {
-
-        width = image.getWidth(null);
-        height = image.getHeight(null);
     }
 
     public Position getPosition(){
@@ -39,9 +19,4 @@ public class Player {
         this.position = position;
     }
 
-    private void initMissile() {
-
-        loadImage("src/rsrc/Player.png");
-        getImageDimensions();
-    }
 }
